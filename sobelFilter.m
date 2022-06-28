cbf8c3b7c7513dcb20f80733c51857c231310f0e
Chu Chen <chenchu@umich.edu>
2022-06-28 15:41:03 -0400

    Delete sobelFilter.m

--- sobelFilter.m
+++
@@ -1,6 +0,0 @@
-function [filtered_im] = sobelFilter(im)
-% Function implements the Sobel Filter to accentuate edges in an image
-%
-hh = fspecial('sobel');
-hv = hh';
-filtered_im = sqrt(imfilter(im, hh).^2 + imfilter(im, hv).^2);
